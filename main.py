688619381b9257c1f3bc2dcde14720fd86343ac1
Brian Zhan <brian.zhan@u.northwestern.edu>
2018-02-02 15:58:42 -0600

    code for finding nominees

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 #! /usr/bin/python
 
-# need function to read tweets, scrape data, pair winners with movies
-
-# award names
-
-# list of years
 import nltk
-from nltk.corpus import stopwords
+from nltk.tag import pos_tag
 from collections import Counter
 import json
 import xml.etree.ElementTree as ElementTree
-from nltk.tag import pos_tag
 
 
 # Motion Picture Awards
@@ -46,84 +40,76 @@ tAward11 = 'Best Supporting Actress in a Series, Limited Series or Motion Pictur
 ignore_list = ['golden', 'globes', 'globe', 'goldenglobes', '#goldenglobes', 'oscars']
 # stopwords = snltk.corpus.stopwords.words('english')
 
-
-def predictWinner(category, tweets, parsedList):
-	if category == mAward1:
-		keywords = ['best', 'picture']
-	res = countMatchingTweets(tweets, keywords)
-
-
-def countMatchingTweets(tweets, keywords):
-	d = defaultdict(int)
-	for i in range(0, len(tweets)):
-		if findMatches(tweets[i], keywords):
-			print("ok")
+# main function for finding the winner in a category
+def findWinner(tweetsList):
+	for i in range(0, 1000):
+		if "Congratulations to" in tweetsList[i]:
+			print tweetsList[i]
+	print "done"
 
 
+# function to find nominees, given keywords that relate to a category
+def findNominee(keywords, tweets):
+	print "entered findNominee"
+	relevantList = relevantTweets(keywords, tweets)
+	nomineeList = {}
+	accurateList = []
+	for tweet in relevantList:
+		print "a tweet is ", tweet
+		tweetUnigram = tweet.split()
+		pnoun = []
+		taggedTweets = pos_tag(tweetUnigram)
+		for word, tag in taggedTweets:
+			print 'tag is ', tag
+			if tag == 'NNP':
+				pnoun.append(word)
+		print "pnoun list is ", pnoun
+		for unigram in tweetUnigram:
+			# idea is find an at to the nominee
+			if unigram[0]=='@' and nomineeList.has_key(unigram):
+				nomineeList[unigram] +=1
+			else:
+				nomineeList[unigram] = 1
+	for nominee in nomineeList:
+		if nomineeList[nominee] > 3:
+			accurateList.append(nominee)
+		else:
+			print "ignored ", nominee
+	return accurateList
+
+# helper function for main(), to extract tweets in a array
+def readTweets():
+	tweetsList = []
+	with open('test1.json') as data_file:
+		tweets = json.load(data_file)
+		for i in range(0, 1000):
+			tweetsList.append(tweets[i]['text'])
+		# print(tweets[0]['text'])
+	return tweetsList
+
+# helper function for relevantTweets()
 def findMatches(tweet, keywords):
     for word in keywords:
         if word not in tweet:
             return False
     return True
 
-
-# each tweet has 2 attributes, tweets[0]['text'] and tweets[0]['id_str']
-# I suspect id_str is useless so will not include it in the array for now
-def readTweets():
-	tweetsList = []
-	with open('test1.json') as data_file:
-		tweets = json.load(data_file)
-		for i in range(0, 50000):
-			tweetsList.append(tweets[i]['text'])
-		# print(tweets[0]['text'])
-	return tweetsList
-
-def getBestMotion(tweetsList):
-	keywords = ['best', 'picture']
-	# d = defaultdict(int)
-	foundone = False
-	for i in range(0, 10):
-		print pos_tag(tweetsList[i].split())
-		if findMatches(tweetsList, keywords):
-			print pos_tag(tweetsList[i]['text'].split())
-			foundone = True
-	print foundone
-
-def stupidGuess(tweetsList):
-	for i in range(0, 50000):
-		if "Congratulations to" in tweetsList[i]:
-			print tweetsList[i]
-	print "done"
-	
-
-
-def getMoviesList():
-	print "get movies"
-	movieList = getMoviesList()
-	tvList = getTvList()
-	presenterList = getPresenterList()
-
-def getMoviesList():
-	tree = ElementTree.parse('2018GGData.xml')
-	root = tree.getroot()
-	body = tree.find('body')
-	print 'body is ', body
-	content_div = body.findall('div')
-	print 'content is ', content_div
-	return []
-
-def getTvList():
-	return []
-
-def getPresenterList():
-	return []
-# Television Awards
+# helper function for findNominee()
+def relevantTweets(keywords, tweetList):
+	relevantList = []
+	for tweet in tweetList:
+		if findMatches(tweet, keywords):
+			print "found match between ", tweet, " and ", keywords
+			relevantList.append(tweet)
+	return relevantList
 
 def main():
 	tweetsList = readTweets()
-	# print len(tweetsList)
-	# print getBestMotion(tweetsList)
-	print stupidGuess(tweetsList)
+	# find nominees
+	nomineeToFind = mAward1
+	if nomineeToFind == mAward1:
+		keywords = ['best']
+		print findNominee(keywords, tweetsList)
 
 if __name__ == "__main__":
 	main()

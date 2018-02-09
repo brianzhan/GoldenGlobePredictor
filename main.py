1d4c480a1f886184c44a72c7657f51fc10d3c684
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-02-09 16:00:14 -0600

    Use ne_chunk to find nominees

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
 
 import nltk
+import operator
 from nltk.tag import pos_tag
 from collections import Counter
 import json
 import xml.etree.ElementTree as ElementTree
-
 
 # Motion Picture Awards
 mAward1 = 'Best Motion Picture - Drama'
@@ -38,81 +38,104 @@ tAward10 = 'Best Supporting Actor in a Series, Limited Series or Motion Picture 
 tAward11 = 'Best Supporting Actress in a Series, Limited Series or Motion Picture made for Television'
 
 ignore_list = ['golden', 'globes', 'globe', 'goldenglobes', '#goldenglobes', 'oscars']
+
+
 # stopwords = snltk.corpus.stopwords.words('english')
 
 # main function for finding the winner in a category
 def findWinner(tweetsList):
-	for i in range(0, 1000):
-		if "Congratulations to" in tweetsList[i]:
-			print tweetsList[i]
-	print "done"
+    for i in range(0, 1000):
+        if "Congratulations to" in tweetsList[i]:
+            print tweetsList[i]
+    print "done"
 
 
 # function to find nominees, given keywords that relate to a category
 def findNominee(keywords, tweets):
-	print "entered findNominee"
-	relevantList = relevantTweets(keywords, tweets)
-	nomineeList = {}
-	accurateList = []
-	for tweet in relevantList:
-		print "a tweet is ", tweet
-		tweetUnigram = tweet.split()
-		pnoun = []
-		taggedTweets = pos_tag(tweetUnigram)
-		for word, tag in taggedTweets:
-			print 'tag is ', tag
-			if tag == 'NNP':
-				pnoun.append(word)
-		print "pnoun list is ", pnoun
-		for unigram in tweetUnigram:
-			# idea is find an at to the nominee
-			if unigram[0]=='@' and nomineeList.has_key(unigram):
-				nomineeList[unigram] +=1
-			else:
-				nomineeList[unigram] = 1
-	for nominee in nomineeList:
-		if nomineeList[nominee] > 3:
-			accurateList.append(nominee)
-		else:
-			print "ignored ", nominee
-	return accurateList
+    print "entered findNominee"
+    relevantList = relevantTweets(keywords, tweets)
+    nomineeList = {}
+    accurateList = []
+    for tweet in relevantList:
+        # print "a tweet is ", tweet
+        tweetUnigram = tweet.split()
+        tweetUnigram = [u.encode('utf-8') for u in tweetUnigram]
+        tweetUnigram = pos_tag(tweetUnigram)
+        pnoun = nltk.ne_chunk(tweetUnigram)
+        pnoun.draw()
+        #
+        # for word, tag in taggedTweets:
+        #     # print 'tag is ', tag
+        #     if tag == 'NNP':
+        #         pnoun.append(word)
+        print "pnoun list is ", pnoun
+        for unigram in tweetUnigram:
+            # idea is find an at to the nominee
+            if nomineeList.has_key(unigram):
+                nomineeList[unigram] += 1
+            else:
+                nomineeList[unigram] = 1
+
+    list_ = [nomineeList[n] for n in nomineeList]
+    list_.sort(reverse=True)
+    threshold = list_[10]
+
+    for nominee in nomineeList:
+        if nomineeList[nominee] > threshold:
+            accurateList.append(nominee)
+        else:
+            print "ignored ", nominee
+
+    return accurateList
+
 
 # helper function for main(), to extract tweets in a array
 def readTweets():
-	tweetsList = []
-	with open('test1.json') as data_file:
-		tweets = json.load(data_file)
-		for i in range(0, 1000):
-			tweetsList.append(tweets[i]['text'])
-		# print(tweets[0]['text'])
-	return tweetsList
+    tweetsList = []
+    with open('test1.json') as data_file:
+        tweets = json.load(data_file)
+        for i in range(len(tweets)):
+            tweet = tweets[i]['text']
+            tweet = tweet.lower()  # convert to lower so our searches aren't case sensitive
+            tweetsList.append(tweet)
+            # print(tweets[0]['text'])
+    return tweetsList
+
 
 # helper function for relevantTweets()
 def findMatches(tweet, keywords):
+    # TODO: don't match against usernames in RTs
+    # e.g. RT @Gregmichael78: Which will have ...
+    # Don't match against RT @Gregmichael78
+    if tweet.startswith('rt @'):
+        tweet = tweet[tweet.find(':') + 1:]
     for word in keywords:
         if word not in tweet:
             return False
     return True
 
+
 # helper function for findNominee()
 def relevantTweets(keywords, tweetList):
-	relevantList = []
-	for tweet in tweetList:
-		if findMatches(tweet, keywords):
-			print "found match between ", tweet, " and ", keywords
-			relevantList.append(tweet)
-	return relevantList
+    relevantList = []
+    for tweet in tweetList:
+        if findMatches(tweet, keywords):
+            # print "found match between ", tweet, " and ", keywords
+            relevantList.append(tweet)
+    # TODO: filter duplicates
+    return list(set(relevantList))
+
 
 def main():
-	tweetsList = readTweets()
-	# find nominees
-	nomineeToFind = mAward1
-	if nomineeToFind == mAward1:
-		keywords = ['best']
-		print findNominee(keywords, tweetsList)
+    tweetsList = readTweets()
+    # find nominees
+    nomineeToFind = mAward1
+    if nomineeToFind == mAward1:
+        keywords = ['best', 'motion', 'picture', 'drama']
+        print findNominee(keywords, tweetsList)
+
 
 if __name__ == "__main__":
-	main()
-	
-# getMoviesList()
+    main()
 
+# getMoviesList()
